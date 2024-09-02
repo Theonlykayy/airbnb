d0a94a1060d6b594aed9d9f1ac31d1d679450cf9
kayy <theonlykayy@gmail.com>
2024-09-02 17:39:13 +0100

    up

--- firebaseauth.js
+++ firebaseauth.js
@@ -1,70 +1,71 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+import {
+  getFirestore,
+  setDoc,
+  doc,
+} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
-import {getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
-import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js"
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBIo7Ucxjjug1RUXuI9LEWeIq4pVE9sHIk",
   authDomain: "airbnb-54cf0.firebaseapp.com",
   projectId: "airbnb-54cf0",
   storageBucket: "airbnb-54cf0.appspot.com",
   messagingSenderId: "201715149293",
-  appId: "1:201715149293:web:a9a4329331cc8688b99fe4"
+  appId: "1:201715149293:web:a9a4329331cc8688b99fe4",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig); 
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
 
-function showMessage(message, divId){
-    var messageDiv=document.getElementById(divId);
-    messageDiv.style.display="block";
-    messageDiv.innerHTML=message;
-    messageDiv.style.opacity=1;
-    setTimeout(function(){
-        messageDiv.style.opacity=0;
-    },5000);
+function showMessage(message, divId) {
+  const messageDiv = document.getElementById(divId);
+  messageDiv.style.display = "block";
+  messageDiv.innerHTML = message;
+  messageDiv.style.opacity = 1;
+  setTimeout(() => {
+    messageDiv.style.opacity = 0;
+  }, 5000);
 }
-const signUp=document.getElementById('submitSignUp');
- signUp.addEventListener('click', (event)=>{
-    event.preventDefault();
-    const email=document.getElementById('rEmail').value;
-    const password=document.getElementById('rPassword').value;
-    const firstName=document.getElementById('fName').value;
 
-    const auth=getAuth();
-    const db=getFirestore();
+document.getElementById("registerForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  handleSignUp();
 });
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential)=>{
-        const user=userCredential.user;
-        const userData={
-            email: email,
-            firstName: firstName,
-            
-        };
-        showMessage('Account Created Successfully', 'signUpMessage');
-        const docRef=doc(db, "users", user.uid);
-        setDoc(docRef,userData)
-        .then(()=>{
-            window.location.href='Login.html';
-        })
+
+document.getElementById("loginForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
+function handleSignUp() {
+  const email = document.getElementById("rEmail").value;
+  const password = document.getElementById("rPassword").value;
+  const firstName = document.getElementById("fName").value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      const userData = {
+        email,
+        firstName,
+      };
+      return setDoc(doc(db, "users", user.uid), userData);
+    })
+    .then(() => {
+      showMessage("Account Created Successfully", "signUpMessage");
+      window.location.href = "Login.html";
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      if (errorCode === "auth/email-already-in-use") {
+        showMessage("Email Address Already Exists !!!", "signUpMessage");
+      } else {
+        showMessage("Unable to create User", "signUpMessage");
+      }
     });
-       
-
-    .catch((error)=>{
-        console.error("error writing document", error);
-
-    });
-
-.catch((error)=>{
-    const errorCode=error.code;
-    if(errorCode=='auth/email-already-in-use'){
-        showMessage('Email Address Already Exists !!!', 'signUpMessage');
-    }
-    else{
-        showMessage('unable to create User', 'signUpMessage');
-    }
-})
+}
