196fcdd90486b342091e270c970d3a3da14c663d
kayy <theonlykayy@gmail.com>
2024-09-02 19:01:21 +0100

    up

--- firebaseauth.js
+++ firebaseauth.js
@@ -68,4 +68,30 @@ function handleSignUp() {
         showMessage("Unable to create User", "signUpMessage");
       }
     });
+
+
+    const signIn=document.getElementById('submitSignIn');
+ signIn.addEventListener('click', (event)=>{
+    event.preventDefault();
+    const email=document.getElementById('email').value;
+    const password=document.getElementById('password').value;
+    const auth=getAuth();
+
+    signInWithEmailAndPassword(auth, email,password)
+    .then((userCredential)=>{
+        showMessage('login is successful', 'signInMessage');
+        const user=userCredential.user;
+        localStorage.setItem('loggedInUserId', user.uid);
+        window.location.href='logged.html';
+    })
+    .catch((error)=>{
+        const errorCode=error.code;
+        if(errorCode==='auth/invalid-credential'){
+            showMessage('Incorrect Email or Password', 'signInMessage');
+        }
+        else{
+            showMessage('Account does not Exist', 'signInMessage');
+        }
+    })
+ })
 }
